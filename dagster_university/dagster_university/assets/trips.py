983ec904d3e66419bc3ee0793d30bad64cb1ef2a
harshraj7795 <hrs7795@gmail.com>
2024-01-29 20:31:11 -0500

    Created asset files for practice

--- dagster_university/dagster_university/assets/trips.py
+++ dagster_university/dagster_university/assets/trips.py
@@ -14,3 +14,15 @@ def taxi_trips_file():
 
     with open(constants.TAXI_TRIPS_TEMPLATE_FILE_PATH.format(month_to_fetch), "wb") as output_file:
         output_file.write(raw_trips.content)
+
+@asset
+def taxi_zones_file():
+    """
+        The raw csv files for the taxi zones dataset. Sourced from the NYC Open Data portal.
+    """
+    raw_taxi_zones = requests.get(
+        "https://data.cityofnewyork.us/api/views/755u-8jsi/rows.csv?accessType=DOWNLOAD"
+    )
+
+    with open(constants.TAXI_ZONES_FILE_PATH, "wb") as output_file:
+        output_file.write(raw_taxi_zones.content)
